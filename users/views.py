72b993b6ce6bf45e6637c059aa547e9b918c823a
autotrade <swedl@yandex.ru>
2023-08-25 17:38:11 +0500

    upgrade loginView

--- users/views.py
+++ users/views.py
@@ -4,12 +4,25 @@ from django.urls import reverse_lazy
 from users.forms import UserLoginForm
 
 
+menu = [{'title': 'Главная', 'url_name': 'carwash:home'},
+        {'title': 'Записаться', 'url_name': 'carwash:registration'},
+        {'title': 'Услуги и цены', 'anchor': '#services_price'},
+        {'title': 'Контакты и адрес', 'anchor': '#footer'},
+        ]
+
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
     form_class = AuthenticationForm #UserLoginForm
 
-    # def get_success_url(self):
-    #     return reverse_lazy('carwash:home')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserLoginView, self).get_context_data()
+
+        user_menu = menu.copy()
+        context['menu'] = [user_menu[0]]
+
+        return context
+
+
 
 
 
