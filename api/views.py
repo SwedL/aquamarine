c0f030786ff2f96289f05f312718a178f074e8cb
Алексей <supersega@mail.ru>
2023-11-11 15:40:24 +0500

    DRF 1.3 upgrade  UserRegistrationsAPI  POST replace DELETE  add Serializer

--- api/views.py
+++ api/views.py
@@ -18,22 +18,19 @@ class CarWashRegistrationAPIView(APIView):
 
 
 class CarWashUserRegistrationAPIView(APIView):
-    serializer_class = CarWashUserRegistrationSerializer
     permission_classes = (IsAuthenticated,)
 
     @staticmethod
-    def get_list_services(request):
-        result_lst = CarWashUserRegistration.objects.filter(client=request.user).values()
-        for i in result_lst:
-            i['carwash_reg_id'] = CarWashRegistration.objects.filter(id=i['carwash_reg_id'])[0].__str__()
-        return sorted(result_lst, key=lambda x: (x['date_reg'], x['time_reg']))
+    def get_response(request):
+        all_user_reg = CarWashUserRegistration.objects.filter(client=request.user)
+        return Response({'user_registrations': CarWashUserRegistrationSerializer(all_user_reg, many=True).data})
 
     def get(self, request):
-        return Response({'user_registration': list(self.get_list_services(request))})
+        return self.get_response(request)
 
-    def post(self, request, registration_pk):
+    def delete(self, request, registration_pk):
         carwash_user_registration_delete(request, registration_pk)
-        return Response({'user_registration': list(self.get_list_services(request))})
+        return self.get_response(request)
 
 
 class CarWashRequestCallCreateAPIView(generics.CreateAPIView):
