f09248afa6159deb9abf550a40b59394c724d28e
osminin <osminin555@bk.ru>
2025-04-23 13:24:22 +0500

    update settings.py README.md base.html

--- project/aquamarine/settings.py
+++ project/aquamarine/settings.py
@@ -157,11 +157,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru-Ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
